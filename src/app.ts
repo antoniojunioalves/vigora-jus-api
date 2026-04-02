ad1b0a5530ec56ec980d78b31796b9d72611414d
antoniojunioalves <antoniojunioalves@gmail.com>
2026-04-02 10:03:19 -0300

    swagger com tela branca

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/api-docs", (_req, res) => {
+  res.redirect(308, "/api-docs/");
+});
+app.use("/api-docs", swaggerUi.serveFiles(swaggerSpec), swaggerUi.setup(swaggerSpec));
 app.get("/api-docs.json", (_req, res) => res.json(swaggerSpec));
 
 app.use("/api", router);
